4b93b1ec842ba75947faba0b4179965de1bbd17f
keshavbansal5766 <keshav22.dlp@gmail.com>
2025-06-22 03:06:11 +0530

    implementing authentication feature

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,9 +1,15 @@
 import Link from "next/link";
 import React from "react";
 import MaxWidthWrapper from "./MaxWidthWrapper";
+import { buttonVariants } from "./ui/button";
+import { ArrowRight } from "lucide-react";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
-const Navbar = () => {
-  const user = undefined;
+const Navbar = async () => {
+  const { getUser } = getKindeServerSession();
+
+  const user = await getUser();
+  const isAdmin = user?.email === process.env.ADMIN_EMAIL;
 
   return (
     // main nav div
@@ -17,6 +23,76 @@ const Navbar = () => {
           {/* Links and also redirecting to logout, login and signup according to authentication */}
           <div className="h-full flex items-center space-x-4">
             {/* UI will change according to user present or not */}
+            {user ? (
+              <>
+                {/* signout functioning */}
+                <Link
+                  href="/api/auth/logout"
+                  className={buttonVariants({
+                    size: "sm",
+                    variant: "ghost",
+                  })}
+                >
+                  Sign out
+                </Link>
+                {/* if admin is available it will show dashboard otherwise nothing*/}
+                {isAdmin ? (
+                  <Link
+                    href="/"
+                    className={buttonVariants({
+                      size: "sm",
+                      variant: "ghost",
+                    })}
+                  >
+                    Dashboard ✨
+                  </Link>
+                ) : null}
+                {/* It will redirect to create case page */}
+                <Link
+                  href="/configure/upload"
+                  className={buttonVariants({
+                    size: "sm",
+                    className: "hidden sm:flex items-center gap-1",
+                  })}
+                >
+                  Create case
+                  <ArrowRight className="size-5 ml-1.5" />
+                </Link>
+              </>
+            ) : (
+              <>
+                {/* if user not available or login so this functioning will come at work */}
+                <Link
+                  href="/api/auth/register"
+                  className={buttonVariants({
+                    size: "sm",
+                    variant: "ghost",
+                  })}
+                >
+                  Sign up
+                </Link>
+                <Link
+                  href="/api/auth/login"
+                  className={buttonVariants({
+                    size: "sm",
+                    variant: "ghost",
+                  })}
+                >
+                  Login
+                </Link>
+                <div className="h-8 w-px bg-zinc-200 hidden sm:block" />
+                <Link
+                  href="/configure/upload"
+                  className={buttonVariants({
+                    size: "sm",
+                    className: "hidden sm:flex items-center gap-1",
+                  })}
+                >
+                  Create case
+                  <ArrowRight className="size-5 ml-1.5" />
+                </Link>
+              </>
+            )}
           </div>
         </div>
       </MaxWidthWrapper>
